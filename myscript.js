023c7f4fbeb35d6b0477d189a72a20babb7c0eb9
leonardopea <leonardo.pea.1996@gmail.com>
2023-05-17 17:26:09 +0200

    risolto il problema di uscita dal ciclo

--- myscript.js
+++ myscript.js
@@ -8,21 +8,9 @@ console.log(anni);
 let km = parseInt (prompt ('Inserisci numero di Km da percorrere'));
 console.log(km);
 
-
-if (anni > 100 || anni < 1) {
-    alert(" Attenzione: il soggetto deve avere un'età compresa tra 1 e 100 anni per viaggiare con noi");
-
-    } else if (isNaN('anni') && isNaN('km')) {
-        alert("Attenzione: non sono stati inseriti dei numeri");
-
-        }else{
-            console.log("continua");
-        }
-        
+// FISSO I  CALCOLI DEI PREZZI//
 
 let prezzo_intero = km * 0.21;
-
-
 
 let prezzo_scontato_junior = prezzo_intero * 80 / 100 ;
 
@@ -31,15 +19,30 @@ let prezzo_scontato_senior = prezzo_intero * 60 / 100;
 
 
 
-if (anni < 18) {
+if (anni > 100 || anni < 1) {
+    alert(" Attenzione: il soggetto deve avere un'età compresa tra 1 e 100 anni per viaggiare con noi");
+
+    } else if (isNaN('anni') && isNaN('km')) {
+        alert("Attenzione: non sono stati inseriti dei numeri");
+
+        }else if (anni < 18) {
                 
-    document.getElementById('prezzo_finale').innerHTML = ` Il tuo biglietto costa: ${prezzo_scontato_junior.toFixed(2)} €`; 
+            document.getElementById('prezzo_finale').innerHTML = ` Il tuo biglietto costa: ${prezzo_scontato_junior.toFixed(2)} €`; 
+        
+            } else if (anni > 65) {
+        
+                document.getElementById('prezzo_finale').innerHTML = `Il tuo biglietto costa: ${prezzo_scontato_senior.toFixed(2)} €`;
+                } else{
+        
+                    document.getElementById('prezzo_finale').innerHTML = `Il tuo biglietto costa: ${prezzo_intero.toFixed(2)} €`;
+                }
+            
+        
+        
 
-    } else if (anni > 65) {
 
-        document.getElementById('prezzo_finale').innerHTML = `Il tuo biglietto costa: ${prezzo_scontato_senior.toFixed(2)} €`;
-        } else{
 
-            document.getElementById('prezzo_finale').innerHTML = `Il tuo biglietto costa: ${prezzo_intero.toFixed(2)} €`;
-        }
+
+
+
     
