3961966971a6e6ce563d1d5eaf495b57860238ba
leonardopea <leonardo.pea.1996@gmail.com>
2023-05-17 17:00:23 +0200

    inserito isnan

--- myscript.js
+++ myscript.js
@@ -3,34 +3,43 @@
 let anni = parseInt (prompt ('Inserisci età del soggetto'));
 console.log(anni);
 
-if (anni >100) {
-    alert(" Attenzione: il soggetto deve avere meno di 100 anni per viaggiare con noi")
-} else{
-    
-    let km = parseInt (prompt ('Inserisci numero di Km da percorrere'));
-    console.log(km);
 
 
-    let prezzo_intero = km * 0.21;
+let km = parseInt (prompt ('Inserisci numero di Km da percorrere'));
+console.log(km);
 
 
+if (anni > 100 || anni < 1) {
+    alert(" Attenzione: il soggetto deve avere un'età compresa tra 1 e 100 anni per viaggiare con noi");
 
-    let prezzo_scontato_junior = prezzo_intero * 80 / 100 ;
+    } else if (isNaN('anni') && isNaN('km')) {
+        alert("Attenzione: non sono stati inseriti dei numeri")
 
-
-    let prezzo_scontato_senior = prezzo_intero * 60 / 100;
-
-
-
-    if (anni < 18) {
+        }else{
+            console.log("continua")
+        }
         
-        document.getElementById('prezzo_finale').innerHTML = ` Il tuo biglietto costa: ${prezzo_scontato_junior.toFixed(2)} €`; 
+
+let prezzo_intero = km * 0.21;
+
+
+
+let prezzo_scontato_junior = prezzo_intero * 80 / 100 ;
+
+
+let prezzo_scontato_senior = prezzo_intero * 60 / 100;
+
+
+
+if (anni < 18) {
+                
+    document.getElementById('prezzo_finale').innerHTML = ` Il tuo biglietto costa: ${prezzo_scontato_junior.toFixed(2)} €`; 
 
     } else if (anni > 65) {
 
         document.getElementById('prezzo_finale').innerHTML = `Il tuo biglietto costa: ${prezzo_scontato_senior.toFixed(2)} €`;
-    } else{
+        } else{
 
-        document.getElementById('prezzo_finale').innerHTML = `Il tuo biglietto costa: ${prezzo_intero.toFixed(2)} €`;
-    }
-}   
+            document.getElementById('prezzo_finale').innerHTML = `Il tuo biglietto costa: ${prezzo_intero.toFixed(2)} €`;
+        }
+    
